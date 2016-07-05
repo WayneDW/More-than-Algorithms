05a15367d99bf884f94ecd6ae88f971cba061fd4
Wei Deng <greyman@live.cn>
2016-07-05 17:42:40 -0400

    No.8 detail question

--- c++/8_String_to_Integer.cpp
+++ c++/8_String_to_Integer.cpp
@@ -2,14 +2,55 @@
 #include<vector>
 #include<map>
 #include<string>
+#include<limits.h>
 
 using namespace std;
 
+class Solution {
+public:
+    int abs(int x) {
+        if (x >= 0)
+            return x;
+        else
+            return -x;
+    }
+    int myAtoi(string str) {
+        int i, j, tag = 0; 
+        int n = str.size();
+        unsigned long long out = 0;
+        // a better way of handling this is to use pointer
+        // but this question are not permitted to do that.
+        for (i = 0; i < n; i++) {
+            if (str[i] != ' ')
+                break; 
+        }
+        if (str[i] == '-')
+            tag = -1; 
+        else if (str[i] == '+')
+            tag += 1;
+        for (j = i + abs(tag); j < n; j++) {
+            if (isdigit(str[j])) {
+                // ascii transformation
+                out = 10 * out + str[j] - '0';
+                if ((tag == -1) & (out > (unsigned long long)INT_MAX + 1))
+                    return INT_MIN;
+                else if ((tag != -1) & (out > (unsigned long long)INT_MAX))
+                    return INT_MAX;
+            }
+            else
+                break;
+        }
+        
+        if (tag == -1)
+            return tag * out;
+        else
+            return out;
+    }
+};
 
 
 
 int main() {
-
-
-
+    Solution s;
+    cout << s.myAtoi("18446744073709551617") << endl;
 }
--- python/8_String_to_Integer.py
+++ python/8_String_to_Integer.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python
 import sys
 import os
+import re
 
 class Solution:
+    def myAtoi(self, str):
+        INT_MAX = 2 ** 31 - 1
+        INT_MIN = -2 ** 31
+        str = str.strip().split(" ")[0]
+        if len(str) == 0:
+            return 0
+        # tag is used for counting the start point
+        tag = 0
+        n = len(str)
+        num = 0
+        if str[0] == '-':
+            tag = -1
+        elif str[0] == '+':
+            tag = 1
+        for i in xrange(abs(tag), n):
+            if str[i].isdigit():
+                num = 10 * num + int(str[i])
+            else:
+                break
+        if tag == -1:
+            return max(tag * num, INT_MIN)
+        else:
+            return min(num, INT_MAX)
+            
+            
+            
 
 
 def main():
     s = Solution()
+    print s.myAtoi("  -0012a41   ")
 
 if __name__ == "__main__":
     main()
