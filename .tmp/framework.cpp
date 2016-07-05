798599d153cbeba4f78884bc537a0a5b6ea86298
Wei Deng <greyman@live.cn>
2016-07-04 22:09:27 -0400

    Folder used for creating frameworks

---
+++ .tmp/framework.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+
+
+
+
+int main() {
+
+
+
+
+}
+
---
+++ .tmp/framework.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys
+
+class Solution(object):
+    
+
+
+
+
+
+
+def main():
+    
+
+
+
+if __name__ == "__main__":
+    main()
