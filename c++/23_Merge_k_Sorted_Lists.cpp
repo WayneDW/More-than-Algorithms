08ea37d830c4abee423498ae80b535b57a57db2a
Wei Deng <greyman@live.cn>
2016-08-16 22:11:08 -0400

    achieved by the idea from merge sort and merge 2 lists,  also covers max heap

--- c++/23_Merge_k_Sorted_Lists.cpp
+++ c++/23_Merge_k_Sorted_Lists.cpp
@@ -6,23 +6,48 @@
 
 using namespace std;
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	void tmp(vector<string> a) {
-		cout << a[0] << endl;
-		cout << a[1] << endl;
-	}
-};
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    	if (lists.size() == 0)
+    		return NULL;
+    	int left, right = lists.size() - 1;
+    	while (right > 0) {
+    		left = 0;
+    		while (left < right)
+    			lists[left] = two_merge(lists[left++], lists[right--]);
+    	}
+    	return lists[0];
+    }
 
+    ListNode* two_merge(ListNode *a, ListNode *b) {
+    	ListNode *head = new ListNode(0);
+    	ListNode *out = head;
+    	while (a && b) {
+    		if (a->val < b->val) {
+    			out->next = a;
+    			a = a->next;
+    		}
+    		else {
+    			out ->next = b;
+    			b = b->next;
+    		}
+    		out = out->next;
+    	}
+    	out->next = a ? a : b;
+    	return head->next;
+    }
+};
 
 int main() {
     Solution s;
     vector<string> ss;
-    string d, dd;
-    d = "(";
-    dd = ")";
-    ss.push_back(d);
-    ss.push_back(dd);
-    s.tmp(ss);
-    
 }
--- python/23_Merge_k_Sorted_Lists.py
+++ python/23_Merge_k_Sorted_Lists.py
@@ -2,7 +2,35 @@
 import sys
 import os
 
-class Solution:
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if len(lists) == 0:
+        	return None
+        right = len(lists) - 1
+        while right > 0:
+        	left = 0
+        	while left < right:
+        		lists[left] = self.two_merge(lists[left], lists[right])
+        		left += 1
+        		right -= 1
+        return lists[0]
+    def two_merge(self, l1, l2):
+    	head = ListNode(0)
+    	out = head
+    	while l1 and l2:
+    		if l1.val < l2.val:
+    			out.next = l1
+    			l1 = l1.next
+    		else:
+    			out.next = l2
+    			l2 = l2.next
+    		out = out.next
+    	out.next = l1 if l1 else l2
+    	return head.next
 
 
 def main():
