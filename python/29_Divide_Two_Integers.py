5abc4c6145ae253b164dd1416bdd5bf9a89b3098
Wei Deng <greyman@live.cn>
2016-08-21 11:26:34 -0400

    overflow  psedo-bit-shift-operator

--- python/29_Divide_Two_Integers.py
+++ python/29_Divide_Two_Integers.py
@@ -2,11 +2,41 @@
 import sys
 import os
 
-class Solution:
-
+class Solution(object):
+    def divide(self, dividend, divisor):
+    	sign = 1
+    	if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
+    		sign = -1
+    	dd, ds = abs(dividend), abs(divisor)
+    	if dd < ds:
+    		return 0
+    	#print 2 ** 4
+    	if dividend == -2 ** 31 and divisor == -1:
+    		return 2 ** 31 - 1
+    	#print dd, ds
+    	res = 0
+    	count = 0
+    	#print "divisor", divisor
+    	while ds <= dd:
+    		count += 1
+    		#print count 
+    		#if count > 100:
+    		#	break
+    		bs_ds = ds # bit shift operator
+    		part = 1
+    		while not (bs_ds <= dd and bs_ds + bs_ds > dd):
+    			bs_ds +=  bs_ds
+    			part += part
+    		#print bs_ds, part
+    		dd -= bs_ds
+    		#print dd
+    		res += part
+    	return res * sign
 
 def main():
     s = Solution()
+    print s.divide(16, 3)
+    print s.divide(-1, 1)
 
 if __name__ == "__main__":
     main()
