786c12c7322a5746376726a61571dca434cfabf4
Wei Deng <greyman@live.cn>
2016-09-09 22:36:56 -0400

    update

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/expect
 
 
 if [ "$#" -ne 2 ] ; then
@@ -6,6 +6,8 @@ if [ "$#" -ne 2 ] ; then
   exit 1
 fi
 
-git add ./python/$1*.py ./c++/$1*.cpp
-git commit -m "$2"
+git add ./python/${1}*.py ./c++/${1}*.cpp
+git commit -m $2
 git push
+expect "Username for 'https://github.com':" { send "waynedw\r"; exp_continue }
+expect "Password for 'https://waynedw@github.com':" { send "dw070703\r"; exp_continue }
