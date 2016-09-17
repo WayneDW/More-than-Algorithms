6cf45be101614fc73902a855ae1fb8db6f843e40
Wei Deng <greyman@live.cn>
2016-09-16 22:56:31 -0400

    unittest update

--- .tmp/template.py
+++ .tmp/template.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
+import unittest
 
 
-def main():
-    s = Solution()
+class Solution(object):
+    pass
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        pass
+
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
