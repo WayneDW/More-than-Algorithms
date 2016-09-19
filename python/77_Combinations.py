fb323311a725cae7804f93fd6e105c354cd497ef
Wei Deng <greyman@live.cn>
2016-09-18 22:24:10 -0400

    great solution example using math trick

--- python/77_Combinations.py
+++ python/77_Combinations.py
@@ -22,15 +22,25 @@ class Solution(object):
 			result.append(res)
 			return
 		for item in range(start, n + 1):
-			''' # this method fails with empty result
-			res.append(item)
-			self.backTracking(item + 1, n, k, p + 1, res, result)
-			res.pop()
-			'''
-			self.backTracking(item + 1, n, k, p + 1, res + [item], result)
-			
+			# this method fails with empty resultsince res changes the structure of result
+			# res.append(item)
+			# self.backTracking(item + 1, n, k, p + 1, res, result)
+			# res.pop()
+			self.backTracking(item + 1, n, k, p + 1, res + [item], result)			
 
+class Solution2:
+	def combine(self, n, k):
+		import itertools
+		return map(list, itertools.combinations(xrange(1, n + 1), k))
 
+# great solution
+class Solution3(object):
+    def combine(self, n, k):
+        if k == 1:
+            return [[i] for i in range(1, n + 1)]
+        if n == k:
+            return [[i for i in range(1, n + 1)]]
+        return [i + [n] for i in self.combine(n - 1, k - 1)] + [i for i in self.combine(n - 1, k)]		
 
 
 
