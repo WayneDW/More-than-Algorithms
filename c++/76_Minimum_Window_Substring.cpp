041162eaafab4d6ccc49d83331d98d4da9aba566
Wei Deng <greyman@live.cn>
2016-10-28 15:02:35 -0400

    tricky double pointer question

--- c++/76_Minimum_Window_Substring.cpp
+++ c++/76_Minimum_Window_Substring.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <string>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-
+class Solution {
+public:
+    string minWindow(string S, string T) {
+        int dting[256] = {}; // use the convenience of ASCII
+        for (int i = 0; i < T.size(); i++)
+            dting[T[i]]++; // count the word frequency in T
+        int lStart = 0, len = 0, cnt = 0, minLen = -1, bestStart = 0;
+        int m = S.size(), n = T.size();
+        for (int i = 0; i < S.size(); i++) {
+            if (dting[S[i]] > 0)
+                cnt++; // cnt == n is used to validify if we have one feasible solution
+            dting[S[i]]--; // used to classify if we want to delete useless words in the boundry 
+            // or surplus words even it is belong to the dict
+            len++; // feasible solution length
+            while (dting[S[lStart]] < 0) { // shrink the interval
+                len--;
+                dting[S[lStart]]++; 
+                lStart++;
+            }
+            if (cnt == n && (len < minLen || minLen == -1)) {
+                minLen = len;
+                bestStart = lStart;
+            }
+        }
+        if (cnt < n) return "";
+        return S.substr(bestStart, minLen);
+    }
+};
 
 
 int main() {
 	Solution s;
+    cout << s.minWindow("adobecodebanc", "abc") << endl;
 }
--- python/76_Minimum_Window_Substring.py
+++ python/76_Minimum_Window_Substring.py
@@ -1,23 +1,83 @@
 #!/usr/bin/python
 import unittest
+import collections
 
+'''
+S = "ADOBECODEBANC"
+T = "ABC"
+Minimum window is "BANC".
+'''
 
+# feasible, but very inefficient
 
+class Solution(object):
+    def minWindow(self, s, t):
+        if s == t: return t
+        dt = {}
+        for i in range(len(t)):
+            dt.setdefault(t[i], 0)
+            dt[t[i]] += 1 # create a dict to count the number of words
+        arr1 = []
+        arr2 = []
+        for num, i in enumerate(range(len(s))):
+            if s[i] in dt:
+                arr1.append(s[i])
+                arr2.append(num) # record the word and location
+        i, j, total = 0, 0, 10 ** 10
+        dt2 = {} # comparision dict
+        res = "" # final result
+        tag = 1
+        while i < len(arr1) or j < len(arr1):
+            if tag and j < len(arr1) and not self.check(dt, dt2): # if dict not match, update dt2
+                dt2.setdefault(arr1[j], 0)
+                dt2[arr1[j]] += 1
+                if j == len(arr1) -1: tag = 0 # avoid repeatly updating dt2
+            if self.check(dt, dt2) and i < len(arr1): 
+                if arr2[j] - arr2[i] < total:
+                    res = s[arr2[i]:arr2[j] + 1]
+                total = min(total, arr2[j] - arr2[i] + 1)
+                dt2[arr1[i]] -= 1
+                if dt2[arr1[i]] == 0: del dt2[arr1[i]]
+                i += 1
+            elif j >= len(arr1) - 1:
+                break
+            if j < len(arr1) - 1 and not (self.check(dt, dt2) and i < len(arr1)):
+                j += 1
 
+        return res
+    def check(self, a, b):
+        for i in a:
+            if i not in b: return False
+            if a[i] > b[i]: return False
+        return True
 
-
-class TestSolution(unittest.TestCase):
-    def setUp(self):
-        self.s = Solution()
-
-    def test_method(self):
-        """Such as self.assertEqual, self.assertTrue"""
-        #self.assertEqual(self.s.)
-        pass
+class Solution2(object):
+    def minWindow(self, s, t):
+        need, missing = collections.Counter(t), len(t)
+        i = I = J = 0
+        for j, c in enumerate(s, 1):
+            missing -= need[c] > 0
+            need[c] -= 1
+            print c, need
+            if not missing:
+                while i < j and need[s[i]] < 0:
+                    need[s[i]] += 1
+                    i += 1
+                if not J or j - i <= J - I:
+                    I, J = i, j
+        return s[I:J]
 
 def main():
     s = Solution()
+    #assert s.minWindow("adobecodebanc", "abc") == "banc"
+    #assert s.minWindow("bba", "ab") == "ba"
+    #assert s.minWindow("cabwefgewcwaefgcf", "cae") == "cwae"
+    print s.minWindow("aabcdb", "bd") 
+    #assert s.minWindow("cabefgecdaecf", "aec") == "aec"
+    #assert s.minWindow("acbba", "aab") == "acbba"
+
+
 
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
