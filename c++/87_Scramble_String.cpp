44f8bdd96b5d11a201926a8029a9bacdd218b5c4
Wei Deng <greyman@live.cn>
2016-10-28 21:39:56 -0400

    recursion

--- c++/87_Scramble_String.cpp
+++ c++/87_Scramble_String.cpp
@@ -8,16 +8,45 @@
 using namespace std;
 
 
-// class Solution {
-// public:
-//     bool isScramble(string s1, string s2) {
-//         return true;
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        if (s1 == s2)
+            return true;
+        if (s1.size() != s2.size())
+            return false;
+        int len = s1.length();
+        // slower version            
+        // string ss1 = s1;
+        // sort(ss1.begin(), ss1.end());
+        // string ss2 = s2;
+        // sort(ss2.begin(), ss2.end());
+        // if (ss1 != ss2)
+        //     return false;
         
-//     }
-// };
+        int cnt[26] = {0};
+        for (int i = 0; i < s1.size(); i++){
+            cnt[s1[i] - 'a']++;
+            cnt[s2[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] != 0)
+                return false;
+        }
 
+        
+        for (int i = 1; i < s1.size(); i++)
+        {
+            if (isScramble(s1.substr(0,i), s2.substr(0,i)) && isScramble(s1.substr(i), s2.substr(i)))
+                return true;
+            if (isScramble(s1.substr(0,i), s2.substr(len-i)) && isScramble(s1.substr(i), s2.substr(0,len-i)))
+                return true;
+        }
+        return false;
+    }
+};
 
 int main() {
-	//Solution s;
-    cout << 1 << endl;
+	Solution s;
+    cout << s.isScramble("a", "b") << endl;
 }
--- python/87_Scramble_String.py
+++ python/87_Scramble_String.py
@@ -2,23 +2,35 @@
 import unittest
 
 
+# https://discuss.leetcode.com/topic/42438/sharing-recursive-python-solution-with-memo-beats-99
+class Solution(object):
+    def isScramble(self, s, t, memo={}):
+        # Check with sorted is fundamental, otherwise TLE
+        if len(s) != len(t) or sorted(s) != sorted(t):
+            return False
+        if len(s) <= 3:
+            return True
+        if s == t:
+            return True
+        if (s, t) in memo:
+            return memo[s, t]
+        n = len(s)
+        for i in range(1, n):
+            a = self.isScramble(s[:i], t[:i], memo) and self.isScramble(s[i:], t[i:], memo)
+            if not a:
+                b = self.isScramble(s[:i], t[-i:], memo) and self.isScramble(s[i:], t[:-i], memo)
+            if a or b:
+                memo[s, t] = True
+                return True
+        memo[s, t] = False
+        return False
 
 
-
-
-
-class TestSolution(unittest.TestCase):
-    def setUp(self):
-        self.s = Solution()
-
-    def test_method(self):
-        """Such as self.assertEqual, self.assertTrue"""
-        #self.assertEqual(self.s.)
-        pass
 
 def main():
     s = Solution()
+    print s.isScramble("rgtae", "great")
 
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
