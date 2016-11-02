1b7d1782591f25db2021abc830409e9761b4e4f8
Wei Deng <greyman@live.cn>
2016-11-02 10:58:22 -0400

    great trick faster pointer and slow pointer

---
+++ c++/141_Linked_List_Cycle.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include "func.cpp"
+
+using namespace std;
+
+class Solution {
+public: // great idea, create two pointers
+//the fast one will approach the slow one one step once
+    bool hasCycle(ListNode *head) {
+        while (!head) return false;
+        ListNode *fast = head, *slow = head;
+        while (slow->next && fast->next) {
+            slow = slow->next;
+            fast = fast->next;
+            if (fast->next) {
+                fast = fast->next;
+                if (fast == slow)
+                    return true;
+            }
+            else
+                break;
+        }
+        return false;
+    }
+};
+
+
+int main() {
+	Solution s;
+    Examples eg;
+    ListNode *n = new ListNode(1);
+    n->next = n;
+    cout << s.hasCycle(n) << endl;
+}
