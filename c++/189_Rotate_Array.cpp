6afbb1fa8ee214a35c8a161dcd491bc08bdf3f49
Wei Deng <greyman@live.cn>
2016-11-06 12:28:28 -0500

    easy vector

---
+++ c++/189_Rotate_Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include "func.cpp"
+
+using namespace std;
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        while (k-- > 0) {
+            int tmp = nums.back();
+            nums.erase(nums.end() - 1);
+            nums.insert(nums.begin(), tmp);
+        }
+    }
+};
+
+int main() {
+	Solution s;
+    Examples eg;
+    int tmp[] = {1,2,3,4,5,6,7};
+    vector<int> vec(tmp, tmp + 7);
+    s.rotate(vec, 3);
+    print1dVector(vec);
+
+}
