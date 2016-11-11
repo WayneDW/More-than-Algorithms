6006cae711dd79013caf263b4368f382fd05aa26
Wei Deng <greyman@live.cn>
2016-11-10 21:13:44 -0500

    DFS DP hardly have no bugs

--- c++/140_Word_Break_II.cpp
+++ c++/140_Word_Break_II.cpp
@@ -18,37 +18,41 @@ public:
         int n = s.size();
         vector<string> res;
         if (!n) return res;
-        //vector<vector<bool> > dp(n+1, vector<bool>(n+1, false));
         vector<bool> dp(n+1, false);
-        dp[0] = true;
-        checkRight(res, s, "", 0, n, wordDict, dp);
-        //print1dVector(dp);
+        dp[0] = true; 
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (dp[j] && wordDict.count(s.substr(j, i-j+1)) > 0) {
+                    dp[i+1] = true;
+                    break; // no need for further steps
+                }
+            }
+        }
+        if(dp[n] == false)  return res; // without this line, time limit exceeded
+        string subStr = "";
+        checkRight(res, s, subStr, 0, n, wordDict, dp);
         return res;
     }
-    void checkRight(vector<string>& res, string s, string subStr, 
+    void checkRight(vector<string>& res, string s, string subStr, // can't be address here, otherwise bug
         int pos, int n, unordered_set<string>& wordDict, vector<bool> &dp) {
         if (pos >= n) {
             subStr.pop_back();
             res.push_back(subStr);
             return;
         }
+        string word = "";
         for (int i = pos; i < n; i++) {
-            for (int len = 1; len <= n - i; len++) {
-                string sub = s.substr(i, len);
-                if (dp[i] && wordDict.count(sub) > 0) {
-                    dp[i+len] = true;
-                    subStr += sub + "_";
-                    checkRight(res, s, subStr, i+len, n, wordDict, dp);
-                    subStr = subStr.substr(0, subStr.size() - sub.size() - 1);
-                    dp[i+len] = false;
-                }
+            word.push_back(s[i]);
+            cout << word << endl;
+            if (dp[i+1] && wordDict.count(word) > 0) {
+                subStr += word + " ";
+                // common mistake is i + 1, here i can already represent length
+                checkRight(res, s, subStr, i + 1, n, wordDict, dp); 
+                subStr = subStr.substr(0, subStr.size() - word.size() - 1);
             }
         }
-
-        
     }
 };
-
 
 int main() {
 	Solution s;
@@ -69,6 +73,7 @@ int main() {
     wordDict.insert("aaaaaaaa");
     wordDict.insert("aaaaaaaaa");
     print1dVector(s.wordBreak("catsanddog", wordDict));
-    print1dVector(s.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", wordDict));
+    string a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    //print1dVector(s.wordBreak(a, wordDict)); // easily time limit exceeded
 
 }
