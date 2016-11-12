9bddc00a267f7d33a4e1576b669acdab1e507db2
Wei Deng <greyman@live.cn>
2016-11-12 13:07:34 -0500

    hash with array is the fastest, use ASCII code

---
+++ c++/387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include "func.cpp"
+
+using namespace std;
+
+class Solution_slow {
+public:
+    int firstUniqChar(string s) {
+        int n = s.size();
+        if (!n) return -1;
+        unordered_map<char, int> dt;
+        for (int i = 0; i < n; i++) {
+            if (dt.count(s[i]) == 0)
+                dt[s[i]] = 1;
+            else
+                dt[s[i]]++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (dt[s[i]] == 1)
+                return i;
+        }
+        return -1;        
+    }
+};
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.size();
+        if (!n) return -1;
+        int dt[26] = {0};
+        for (int i = 0; i < n; i++)
+            dt[s[i] - 'a']++;
+        for (int i = 0; i < n; i++) {
+            if (dt[s[i] - 'a'] == 1)
+                return i;
+        }
+    }
+};
+
+int main() {
+	Solution s;
+    Examples eg;
+}
