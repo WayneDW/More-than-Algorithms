3c43dccfe60fe9d49c385fc90c507f76db49ac2c
Wei Deng <greyman@live.cn>
2016-11-13 12:24:01 -0500

    buffer

--- c++/157_Read_N_Characters_Given_Read4.cpp
+++ c++/157_Read_N_Characters_Given_Read4.cpp
@@ -42,18 +42,18 @@ public:
     }
 };
 
-class Solution1 {
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
 public:
     int read(char *buf, int n) {
-        int res = 0;
-        for (int i = 0; i <= n / 4; ++i) {
-            int cur = read4(buf + res);
-            if (cur == 0) break;
-            res += cur;
-        }
+        int res = 0, len = n / 4 + 1;
+        while (len--) res += read4(buf + res);
         return min(res, n);
     }
 };
+
 
 int main() {
 	Solution s;
