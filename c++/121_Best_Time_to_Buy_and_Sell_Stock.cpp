7ae222782a97b0b9c9e9525f95b8ddf7ce828219
Wei Deng <greyman@live.cn>
2016-11-13 20:54:51 -0500

    easy

--- c++/121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ c++/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -9,6 +9,18 @@
 
 using namespace std;
 
+/*
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
