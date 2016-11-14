68054d38b0028517e709bddc523a7aafcf1fc0e0
Wei Deng <greyman@live.cn>
2016-11-13 23:27:21 -0500

    overflow math

--- c++/007_Reverse_Integer.cpp
+++ c++/007_Reverse_Integer.cpp
@@ -1,9 +1,13 @@
-#include<iostream>
-#include<vector>
-#include<cmath>
-#include<limits.h>
-#include<map>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include "func.cpp"
 
 using namespace std;
 
