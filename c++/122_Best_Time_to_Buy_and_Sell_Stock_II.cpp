dcd190900f9df2502ee7208327ce51f5998bb6c1
Wei Deng <greyman@live.cn>
2016-11-13 20:55:13 -0500

    iterative sum

--- c++/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ c++/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -10,16 +10,16 @@
 using namespace std;
 
 /*
-Example 1:
-Input: [7, 1, 5, 3, 6, 4]
-Output: 5
+122. Best Time to Buy and Sell Stock II
+Say you have an array for which the ith element is the price of a given stock on day i.
 
-max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
-Example 2:
-Input: [7, 6, 4, 3, 1]
-Output: 0
+Design an algorithm to find the maximum profit. You may complete as many transactions 
+as you like (ie, buy one and sell one share of the stock multiple times). However, you 
+may not engage in multiple transactions at the same time (ie, you must sell the stock 
+before you buy again).
+*/
 
-In this case, no transaction is done, i.e. max profit = 0.*/
+// as long as profit comes, add to total profit.
 
 class Solution {
 public:
