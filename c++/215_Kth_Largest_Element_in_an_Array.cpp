935d22e5c0af7f7dfa2fab92c155d3d1dbacd943
Wei Deng <greyman@live.cn>
2016-11-13 23:43:26 -0500

    add priority queue similar to heap

--- c++/215_Kth_Largest_Element_in_an_Array.cpp
+++ c++/215_Kth_Largest_Element_in_an_Array.cpp
@@ -38,8 +38,6 @@ public:
         heapSize = nums.size();
         for (int i = (heapSize >> 1) - 1; i >= 0; i--)
             heapify(nums, i);
-        cout << " ** ";
-        print1dVector(nums);
         // pop k-1 times
         for (int i = 0; i < k - 1; i++)
             heapPop(nums);
@@ -67,6 +65,15 @@ private:
 };
 
 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> q(nums.begin(), nums.end());
+        for (int i = 0; i < k - 1; i++)
+            q.pop();
+        return q.top();
+    }
+}; 
 
 
 int main() {
