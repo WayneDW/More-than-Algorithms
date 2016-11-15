14eeb3505fb041e3e5973d4ceacc7ed8c6301224
Wei Deng <greyman@live.cn>
2016-11-15 16:04:22 -0500

    almost the way as 15

--- c++/016_3Sum_Closest.cpp
+++ c++/016_3Sum_Closest.cpp
@@ -16,28 +16,28 @@ using namespace std;
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-    	sort(nums.begin(), nums.end());
-    	int out = 0;
-    	int MIN = pow(10, 8);
-    	int len = nums.size();
-    	for (int i = 0; i < len - 2; i++){
-    		int left = i + 1;
-    		int right = len - 1;
-    		while (left < right) {
-    			int total = nums[i] + nums[left] + nums[right];
-    			int diff = abs(total - target);
-    			if (total == target)
-    				return target;
-    			else if (total < target)
-    				left += 1;
-    			else
-    				right -= 1;
-    			if (diff < MIN) {
-    				MIN = diff;
-    				out = total;
-    			}
-    		}
-    	}
+        sort(nums.begin(), nums.end());
+        int out = 0;
+        int MIN = INT_MAX;
+        int len = nums.size();
+        for (int i = 0; i < len - 2; i++){
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                int total = nums[i] + nums[left] + nums[right];
+                int diff = abs(total - target);
+                if (total == target)
+                    return target;
+                else if (total < target)
+                    left += 1;
+                else
+                    right -= 1;
+                if (diff < MIN) {
+                    MIN = diff;
+                    out = total;
+                }
+            }
+        }
         return out;
     }
 };
