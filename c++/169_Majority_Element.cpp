17dbf858d7f0e1b2855fcf5249c3ece81a6b7df0
Wei Deng <greyman@live.cn>
2016-11-20 18:53:44 -0500

    multiple methods, hash, sort, divide and conquer

---
+++ c++/169_Majority_Element.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+class Solution_sort {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
+
+class Solution_hashtable { // faster
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> counts; 
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (++counts[nums[i]] > n / 2)
+                return nums[i];
+        }
+        return 0;
+    }
+};
+
+
+// Moore Voting Algorithm, just works for this question
+
+// A brilliant and easy-to-implement algorithm! It also runs very fast, about 20ms.
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int major, counts = 0, n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (!counts) {
+                major = nums[i];
+                counts = 1;
+            }
+            else counts += (nums[i] == major) ? 1 : -1;
+        }
+        return major;
+    }
+};
+int main() {
+	Solution s;
+    Examples eg;
+}
