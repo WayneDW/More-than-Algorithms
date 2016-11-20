9ef8ef52228a0204887f7d105e6028bda76e510b
Wei Deng <greyman@live.cn>
2016-11-20 10:52:48 -0500

    math

--- c++/009_Palindrome_Number.cpp
+++ c++/009_Palindrome_Number.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<cmath>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
