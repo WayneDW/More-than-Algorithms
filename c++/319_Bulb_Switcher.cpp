154fd6b27d4f6210d7c3b9b96534824db498ca9c
Wei Deng <greyman@live.cn>
2016-12-01 22:45:35 -0500

    brainteaser

---
+++ c++/319_Bulb_Switcher.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+/*  bulbs ends up on iff it is switched an odd times 
+    if not a square, it must be switched even times
+    since 15 = 3 * 5 = 5 * 3, every factorization have a pair
+*/
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+
+
+int main() {
+    Solution s;
+    Examples eg;
+}
