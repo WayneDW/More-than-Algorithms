1bf463d131331ccc42fcf107c87e0faf7a8dad9a
Wei Deng <greyman@live.cn>
2016-12-01 20:58:09 -0500

    easy

---
+++ c++/412_Fizz_Buzz.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) res.push_back("FizzBuzz");
+            else if (i % 3 == 0) res.push_back("Fizz");
+            else if (i % 5 == 0) res.push_back("Buzz");
+            else res.push_back(to_string(i));
+        }
+        return res;
+    }
+};
+
+
+int main() {
+    Solution s;
+    Examples eg;
+}
