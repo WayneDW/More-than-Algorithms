6c40d1d672d0884a5b00c610a3b940440cb3d773
WayneDW <greyman@live.cn>
2016-12-03 23:54:39 -0500

    slow fast find middle, reverse list and compare

--- c++/234_Palindrome_Linked_List.cpp
+++ c++/234_Palindrome_Linked_List.cpp
@@ -25,40 +25,40 @@ using namespace std;
 
 
 /*
-	Use two pointers to reach the middle point
-	then reverse linked list, the fast pointer keeps going, 
-	the slow return the opposite direction
+    Use two pointers to reach the middle point
+    then reverse linked list, the fast pointer keeps going, 
+    the slow return the opposite direction
 
 */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-    	if(!head || !head->next)
+        if(!head || !head->next)
             return true;
-    	auto slow = head, fast = head;
-    	while (fast->next && fast->next->next) {
-    		slow = slow->next;
-    		fast = fast->next->next;
-    	}
-    	slow = reverseLinkedList(slow->next);
-    	while (slow) {
-    		if (slow->val != head->val) return false;
-    		slow = slow->next;
-    		head = head->next;
-    	}
-    	return true;
+        auto slow = head, fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        slow = reverseLinkedList(slow->next);
+        while (slow) {
+            if (slow->val != head->val) return false;
+            slow = slow->next;
+            head = head->next;
+        }
+        return true;
     }
 
     ListNode* reverseLinkedList(ListNode* head) {
-    	ListNode *next = NULL;
-    	ListNode *prev = NULL;
-    	while (head) {
-    		next = head->next;
-    		head->next = prev;
-    		prev = head;
-    		head = next;
-    	}
-    	return prev;
+        ListNode *next = NULL;
+        ListNode *prev = NULL;
+        while (head) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 };
 
