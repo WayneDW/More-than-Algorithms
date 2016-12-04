aff673bc0f41b0d647089a8c33eaa8149e1a8bda
WayneDW <greyman@live.cn>
2016-12-04 10:33:06 -0500

    utilize the property of binary search tree

---
+++ c++/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+/*      _______6______
+       /              \
+    ___2__          ___8__
+   /      \        /      \
+   0      _4       7       9
+         /  \
+         3   5
+For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another 
+example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+         */
+
+// Remember, this is a binary search tree
+
+class Solution { // recursion
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > root->val && q->val > root->val)
+        	return lowestCommonAncestor(root->right, p, q);
+        else if (p->val < root->val && q->val < root->val)
+        	return lowestCommonAncestor(root->left, p, q);
+        return root;
+    }
+};
+
+
+class Solution {  // iteration
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        auto cur = root;
+        while (1) {
+            if (p -> val > cur -> val && q -> val > cur -> val)
+                cur = cur -> right;
+            else if (p -> val < cur -> val && q -> val < cur -> val)
+                cur = cur -> left;
+            else return cur; 
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    Examples eg;
+}
