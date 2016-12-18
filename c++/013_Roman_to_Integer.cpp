adb5e5bbfdfd50a67bb47ddb3ac52b03ee996620
WayneDW <greyman@live.cn>
2016-12-18 17:58:56 -0500

    know roman digit rules

--- c++/013_Roman_to_Integer.cpp
+++ c++/013_Roman_to_Integer.cpp
@@ -9,10 +9,10 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
-    	int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-		string numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}; // 13
         int i = 0;
-		int out = 0;
+        int out = 0;
         int start = 0;
         while (i < 13 && start < s.size()){
             string tmp = s.substr(start, numerals[i].size());
@@ -20,13 +20,29 @@ public:
                 start += numerals[i].size();
                 out += values[i];
             }
-            else {
-                i += 1;
-            }
+            else i++;
         }
-		return out;
+        return out;
     }
 };
+
+int romanToInt(string s) 
+{
+    unordered_map<char, int> T = { { 'I' , 1 },
+                                   { 'V' , 5 },
+                                   { 'X' , 10 },
+                                   { 'L' , 50 },
+                                   { 'C' , 100 },
+                                   { 'D' , 500 },
+                                   { 'M' , 1000 } };
+                                   
+   int sum = T[s.back()];
+   for (int i = s.length() - 2; i >= 0; --i) {
+       if (T[s[i]] < T[s[i + 1]]) sum -= T[s[i]];
+       else sum += T[s[i]];
+   }
+   return sum;
+}
 
 
 int main() {
