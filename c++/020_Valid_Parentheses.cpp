e5decaf05bc4ab4fc585413384240833be25287c
WayneDW <greyman@live.cn>
2016-12-17 22:46:16 -0500

    stack classical example

--- c++/020_Valid_Parentheses.cpp
+++ c++/020_Valid_Parentheses.cpp
@@ -11,7 +11,6 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '(' || s[i] == '[' || s[i] == '{')
                 stk.push(s[i]);
