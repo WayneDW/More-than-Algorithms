01f827f4a30504c9b38c37947a7c5ce5fddf41a0
WayneDW <greyman@live.cn>
2016-12-17 22:36:08 -0500

    overflow

--- c++/007_Reverse_Integer.cpp
+++ c++/007_Reverse_Integer.cpp
@@ -14,20 +14,7 @@ using namespace std;
 class Solution {
 public:
     int reverse(int x) {
-        /*
-        int sign, out;
-        if (x > 0)
-            sign = 1;
-        else
-            sign = -1;
-        x = abs(x);
-        while (x) {
-            out = 10 * out + x % 10;
-            x /= 10;
-        }
-        return sign * out;
-        */
-        int  out = 0;
+        int out = 0;
         while (x){
             // Take care of overflow
             if (out > INT_MAX/10 || out < INT_MIN/10){
