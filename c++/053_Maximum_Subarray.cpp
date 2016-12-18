1e69e74eaa5a4cd1cea8dc7f84bb02389308d428
WayneDW <greyman@live.cn>
2016-12-17 20:23:25 -0500

    dynamic programming

--- c++/053_Maximum_Subarray.cpp
+++ c++/053_Maximum_Subarray.cpp
@@ -7,18 +7,18 @@
 using namespace std;
 
 class Solution {
-public: // dp[i] : sum of maximum value ended at i
-    int maxSubArray(vector<int>& num) {
-    	int n = num.size();
-    	if (n == 0)
-    		return NULL;
-    	int maxS = num[0];
-    	int dp[n] = {0};
-    	dp[0] = num[0];
-    	for (int i = 1; i < n; i++){
-    		dp[i] = dp[i - 1] > 0 ? dp[i - 1] + num[i] : num[i];
-    		maxS = max(maxS, dp[i]);
-    	}
-    	return maxS;
+public:
+    int maxSubArray(vector<int>& nums) {
+        // dp[i]: maximum value from left to i
+        int n = nums.size();
+        if (n == 0) return -1;
+        int dp[n] = {0};
+        dp[0] = nums[0];
+        int maxN = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = max(dp[i-1] + nums[i], nums[i]);
+            maxN = max(maxN, dp[i]);
+        }
+        return maxN;
     }
 };
