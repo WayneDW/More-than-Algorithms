88499cf3153ba71f065327d56a5d4461118f891d
WayneDW <greyman@live.cn>
2016-12-18 12:51:37 -0500

    simplified version update from back to front

--- c++/088_Merge_Sorted_Array.cpp
+++ c++/088_Merge_Sorted_Array.cpp
@@ -16,10 +16,19 @@ public:
     }
 };
 
+
+class Solution { // simplified version
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, tar = m + n - 1;
+        while (j >= 0) nums1[tar--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+    }
+};
+
 int main() {
-	Solution s;
-	vector<int> nums1;
-	vector<int> nums2;
-	nums1.push_back(1);
-	s.merge(nums1, 1, nums2, 0);
+    Solution s;
+    vector<int> nums1;
+    vector<int> nums2;
+    nums1.push_back(1);
+    s.merge(nums1, 1, nums2, 0);
 }
