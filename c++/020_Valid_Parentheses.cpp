430e0a45b372ca2fc07644ca3e20d68c74175f38
WayneDW <greyman@live.cn>
2016-12-17 22:14:08 -0500

    stack classical example

--- c++/020_Valid_Parentheses.cpp
+++ c++/020_Valid_Parentheses.cpp
@@ -13,25 +13,25 @@ public:
         stack<char> stk;
 
         for (int i = 0; i < s.size(); i++) {
-        	if (s[i] == '(' || s[i] == '[' || s[i] == '{')
-        		stk.push(s[i]);
-        	else {
-        		if (stk.empty() || unmatch(stk.top(), s[i]))
-        			return false;
-        		stk.pop();
-        	}
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                stk.push(s[i]);
+            else {
+                if (stk.empty() || unmatch(stk.top(), s[i]))
+                    return false;
+                stk.pop();
+            }
         }
         return stk.empty();
     }
     bool unmatch(char a, char b) {
-    	if (a == '(')
-    		return (b != ')');
-    	else if (a == '[')
-    		return (b != ']');
-    	else if (a == '{')
-    		return (b != '}');
-    	else
-    		return false;
+        if (a == '(')
+            return (b != ')');
+        else if (a == '[')
+            return (b != ']');
+        else if (a == '{')
+            return (b != '}');
+        else
+            return false;
     }
 };
 
