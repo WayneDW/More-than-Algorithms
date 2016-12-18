384a4ed95818ff22350be0905f0e236ab318bc42
WayneDW <greyman@live.cn>
2016-12-17 21:04:58 -0500

    recursion overflow

--- c++/050_Pow.cpp
+++ c++/050_Pow.cpp
@@ -7,30 +7,49 @@
 using namespace std;
 
 
-class Solution {
+class Solution2 {
 public:
     double myPow(double x, int n) {
-    	if (n == 0)
-    		return 1;
-    	if (n < 0) {
-    		if (n == -pow(2, 31))
-    			return x * myPow(x, n + 1);
-    		return 1 / myPow(x, -n);
-    	}
-    	if (n == 1)
-    		return x;
-    	double y = myPow(x, n / 2);
-    	y *= y;
-    	if (n % 2 == 1)
-    		y *= x;
-    	return y;
+        if (n == 0)
+            return 1;
+        if (n < 0) {
+            if (n == -pow(2, 31))
+                return x * myPow(x, n + 1);
+            return 1 / myPow(x, -n);
+        }
+        if (n == 1)
+            return x;
+        double y = myPow(x, n / 2);
+        y *= y;
+        if (n % 2 == 1)
+            y *= x;
+        return y;
     }
 };
 
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n < 0) {
+            if (n == -pow(2, 31)) return 1 / myPow(x, INT_MAX) / x;
+            return 1 / myPow(x, -n);
+        }
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        int i = 1;
+        double out = x;
+        while ((unsigned long long)(i * 2) < n) {
+            out *= out;
+            i *= 2;
+        }
+        cout << out << endl;
+        return out * myPow(x, n - i);
+    }
+};
 
 int main() {
-	Solution s;
-	cout << s.myPow(-1, -2147483648) << endl;
-
-
+    Solution s;
+    // cout << s.myPow(-1, -2147483648) << endl;
+    cout << (pow(2, 31) % 10) << endl;
+    // cout << s.myPow(2, 2147483647) << endl;
 }
