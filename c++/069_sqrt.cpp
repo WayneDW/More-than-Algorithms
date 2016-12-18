5bd68a4399939df913718e5897b89daf9a80eb7e
WayneDW <greyman@live.cn>
2016-12-17 21:47:06 -0500

    compare mid with x divide by mid can avoid overflow

--- c++/069_sqrt.cpp
+++ c++/069_sqrt.cpp
@@ -3,10 +3,12 @@
 #include <map>
 #include <string>
 #include <algorithm>
+#include <climits>
+#include <cmath>
 
 using namespace std;
 
-class Solution {
+class Solution2 {
 public:
     int mySqrt(int x) {
     	if (x < 1)
@@ -27,9 +29,24 @@ public:
     }
 };
 
-
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 1) return 0;
+        else if (x < 4) return 1;
+        int left = 0, right = x;
+        while (1) {
+            int mid = left + (right - left) / 2;
+            if (mid > x / mid) right = mid - 1;
+            else {
+                if ((mid + 1) > x / (mid + 1)) return mid;
+                left = mid + 1;
+            }
+        }
+    }
+};
 
 int main() {
 	Solution s;
-	cout << s.mySqrt(2147395599) << endl;
+	cout << s.mySqrt(pow(2, 31) - 1) << endl;
 }
