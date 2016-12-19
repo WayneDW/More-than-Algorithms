57d91e96c09df5d3b8d10d34e58b912117def1b1
WayneDW <greyman@live.cn>
2016-12-18 21:13:54 -0500

    hashtable

--- c++/003_Longest_Substring_Without_Repeating_Characters.cpp
+++ c++/003_Longest_Substring_Without_Repeating_Characters.cpp
@@ -4,7 +4,7 @@ public:
         int last = -1;
         int maximum = 0;
         int n = s.size();
-        map<char, int> dict;
+        unordered_map<char, int> dict;
         for (int i = 0; i < n; i++) {
             if ((dict.count(s[i]) != 0) && (last < dict[s[i]])) last = dict[s[i]];
             dict[s[i]] = i;
