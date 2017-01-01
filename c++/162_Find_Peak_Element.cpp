ad23f25659fd6a56e94c88b2b69ffdc95f1083c0
WayneDW <greyman@live.cn>
2017-01-01 11:58:08 -0500

    local optimal

---
+++ c++/162_Find_Peak_Element.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+};
+
+
+int main() {
+    Solution s;
+    Examples eg;
+}
