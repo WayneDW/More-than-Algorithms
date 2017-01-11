19db9ca4386bbfd45d92f91c76fc1941f99242ef
WayneDW <greyman@live.cn>
2017-01-10 22:52:32 -0500

    do not fully understand, more oop is required

---
+++ c++/289_Game_of_Life.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+
+
+
+int main() {
+    Solution s;
+    Examples eg;
+}
