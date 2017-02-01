45846f43661a0a1392eb4258ae8b91ffa96df3eb
WayneDW <greyman@live.cn>
2017-02-01 11:25:31 -0500

    dfs

--- c++/437_Path_Sum_III.cpp
+++ c++/437_Path_Sum_III.cpp
@@ -59,21 +59,6 @@ private:
 int main() {
     Solution s;
     Examples eg;
-    // TreeNode* t0 = new TreeNode(10);
-    // TreeNode* t1 = new TreeNode(5);
-    // TreeNode* t2 = new TreeNode(-3);
-    // TreeNode* t3 = new TreeNode(3);
-    // TreeNode* t4 = new TreeNode(2);
-    // TreeNode* t5 = new TreeNode(11);
-    // TreeNode* t6 = new TreeNode(3);
-    // TreeNode* t7 = new TreeNode(-2);
-    // TreeNode* t8 = new TreeNode(1);
-    // t0->left = t1; t0->right = t2;
-    // t1->left = t3; t1->right = t4;
-    // t2->right = t5;
-    // t3->left = t6; t3->right = t7;
-    // t4->right = t8;
-    // cout << s.pathSum(t0, 8) << endl;
 
     TreeNode* tt0 = new TreeNode(1);
     TreeNode* tt1 = new TreeNode(2);
