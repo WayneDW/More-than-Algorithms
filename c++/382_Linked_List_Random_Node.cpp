3b2e6179493939db36559a0b560c6ee6ed1d6e3e
WayneDW <greyman@live.cn>
2017-02-21 23:20:17 -0500

    reservoir sampling

---
+++ c++/382_Linked_List_Random_Node.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <bitset>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+/*// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    ListNode* head;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int i = 2;
+        int res = head->val;
+        ListNode* node = head->next; // use node we can use function getRandom() repeatedly
+        while (node) {
+            int j = rand() % i;
+            if (j == 0) res = node->val;
+            i++;
+            node = node->next;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+
+
+int main() {
+    Solution s;
+    Examples eg;
+}
