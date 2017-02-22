08b1a14c758b4e819699bcf68efda32516234a4d
WayneDW <greyman@live.cn>
2017-02-21 22:31:25 -0500

    design stack

---
+++ c++/341_Flatten_Nested_List_Iterator.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <bitset>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+/*Given a nested list of integers, implement an iterator to flatten it.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+Given the list [[1,1],2,[1,1]],
+
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+
+Example 2:
+Given the list [1,[4,[6]]],
+
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class NestedIterator {
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stk.push(nestedList[i]);
+        }
+    }
+
+    int next() {
+        if (hasNext()) {
+            int res = stk.top().getInteger();
+            stk.pop();
+            return res;
+        }
+    }
+
+    bool hasNext() {
+        while (!stk.empty()) {
+            NestedInteger cur = stk.top();
+            if (cur.isInteger()) {
+                return true;
+            }
+            else {
+                stk.pop();
+                for (int i = cur.getList().size() - 1; i >= 0; i--) {
+                    stk.push(cur.getList()[i]);
+                }
+            }
+        }
+        return false;
+    }
+private:
+    stack<NestedInteger> stk;
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
+
+
+int main() {
+    Solution s;
+    Examples eg;
+}
