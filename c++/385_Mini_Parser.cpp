9bd0d41bd633c11a671dcb9979ae2df0c4897db8
WayneDW <greyman@live.cn>
2017-03-03 23:11:58 -0500

    stack

---
+++ c++/385_Mini_Parser.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <bitset>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+/*Given a nested list of integers represented as a string, implement a parser to deserialize it.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Note: You may assume that the string is well-formed:
+
+String is non-empty.
+String does not contain white spaces.
+String contains only digits 0-9, [, - ,, ].
+Example 1:
+
+Given s = "324",
+
+You should return a NestedInteger object which contains a single integer 324.
+Example 2:
+
+Given s = "[123,[456,[789]]]",
+
+Return a NestedInteger object containing a nested list with 2 elements:
+
+1. An integer containing value 123.
+2. A nested list containing two elements:
+    i.  An integer containing value 456.
+    ii. A nested list with one element:
+         a. An integer containing value 789.*/
+
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class Solution {    
+public:
+    NestedInteger deserialize(string s) {
+        stack<NestedInteger> stk;
+        int sign = 1, num = 0;
+        bool ifHas = false;
+        for (auto c: s) {
+            if (c == '-') sign = -1;
+            else if (isdigit(c)) {
+                ifHas = true;
+                num = 10 * num + c - '0';
+            }
+            else if (c == '[') {
+                stk.push(NestedInteger());
+            }
+            else { // for cases of , and ]
+                if (ifHas) stk.top().add(sign * num);
+                num = 0, sign = 1, ifHas = false;
+                if (c == ']' && stk.size() > 1) {
+                    NestedInteger item = stk.top(); stk.pop();
+                    stk.top().add(item);
+                }
+            }
+        }
+        if (ifHas) return NestedInteger(sign * num); // when there is just a single digit
+        return stk.top();
+    }
+};
+
+
+int main() {
+    Solution s;
+    Examples eg;
+}
