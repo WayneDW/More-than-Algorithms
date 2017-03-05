0302f55e125e225c1451a1f271c2de76a7d724ce
WayneDW <greyman@live.cn>
2017-03-05 18:15:25 -0500

    math greatest common divisor

---
+++ c++/365_Water_and_Jug_Problem.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <bitset>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <climits>
+#include "000_basic.cpp"
+
+using namespace std;
+
+/*You are given two jugs with capacities x and y litres. There is an infinite amount of water supply available. 
+You need to determine whether it is possible to measure exactly z litres using these two jugs.
+
+If z liters of water is measurable, you must have z liters of water contained within one or both buckets by the end.
+
+Operations allowed:
+
+Fill any of the jugs completely with water.
+Empty any of the jugs.
+Pour water from one jug into another till the other jug is completely full or the first jug itself is empty.
+Example 1: (From the famous "Die Hard" example)
+
+Input: x = 3, y = 5, z = 4
+Output: True
+Example 2:
+
+Input: x = 2, y = 6, z = 5
+Output: False*/
+
+class Solution {
+public:
+    bool canMeasureWater(int x, int y, int z) {
+        if (z > x + y) return false;
+        if (z == x || z == y || z == x + y) return true;
+        return z % gcd(x, y) == 0;
+    }
+
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+};
+
+
+int main() {
+    Solution s;
+    Examples eg;
+}
