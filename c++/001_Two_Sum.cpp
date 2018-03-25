15cd4eaba6c271f7578dbd7dd25b9eab13bf8370
Wei Deng <greyman@live.cn>
2018-03-25 11:09:55 -0400

    Update 001_Two_Sum.cpp
    the past version is too bulky.

--- c++/001_Two_Sum.cpp
+++ c++/001_Two_Sum.cpp
@@ -19,37 +19,20 @@ return [0, 1].
 take care of [4, 4], target = 8
 */
 
-
-
 class Solution {
 public:
-    vector<int> twoSum(vector<int> nums, int target) {
-        int i, index1, index2, tmp;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
         vector<int> vec;
-        map<int, vector<int>> myMap;
-        for (i = 0; i < nums.size(); i++)
-            myMap[nums[i]].push_back(i); // repeat values
-        index1 = -1;
-        index2 = -1;
-        for (i = 0; i < nums.size(); i++) {
-            tmp = target - nums[i];
-            if (myMap.count(tmp) > 0) {
-                if (tmp != nums[i]) {
-                    index1 = i;
-                    index2 = myMap[tmp][0];
-                    break;
-                }
-                // need to judge if size > 1 otherwise 6 - 3 could work unexpected
-                else if (myMap[tmp].size() > 1) {
-                    index1 = myMap[tmp][0];
-                    index2 = myMap[tmp][1];
-                    break;
-                }
+        for (int i = 0; i < nums.size(); i++) {
+            int left = target - nums[i];
+            if (mp.find(left) != mp.end()) {
+                vec.push_back(i);
+                vec.push_back(mp[left]);
+                return vec;
             }
-            
+            mp[nums[i]] = i;
         }
-        vec.push_back(index1);
-        vec.push_back(index2);
         return vec;
     }
 };
