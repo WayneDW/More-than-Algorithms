21e509318eb603ba58dd873dc0ebcf0626c07533
Wei Deng <greyman@live.cn>
2018-03-25 17:40:27 -0400

    Update 110_Balanced_Binary_Tree.cpp

--- c++/110_Balanced_Binary_Tree.cpp
+++ c++/110_Balanced_Binary_Tree.cpp
@@ -14,19 +14,19 @@ using namespace std;
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        if (!root) return true;
-        return checkDFS(root);
-    }
-    int maxDepth(TreeNode* root) {
-        if (!root) return 0;
-        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
-    }
-    bool checkDFS(TreeNode* root) { // postorder traverse
+        return dfs(root);
+    }   
+    bool dfs(TreeNode* root) {
         if (!root) return true;
         int l = maxDepth(root->left);
         int r = maxDepth(root->right);
-        if (abs(l - r) > 1) return false;
-        else return checkDFS(root->left) && checkDFS(root->right);
+        if ( abs(l - r) > 1) return false;
+        else return dfs(root->left) && dfs(root->right);
+    }
+    
+    int maxDepth(TreeNode * root) {
+        if (!root) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
 
