173ba6570f422c411c7c60bafba5b9bc28f850aa
Wei Deng <greyman@live.cn>
2018-03-26 17:17:36 -0400

    Update 069_sqrt.cpp

--- c++/069_sqrt.cpp
+++ c++/069_sqrt.cpp
@@ -46,6 +46,26 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    int mySqrt(int x) {
+    	if (x < 1)
+    		return 0;
+    	int l = 0, r = x;
+    	while (l <= r) {
+    		long int mid = (l + r) / 2;
+    		if (mid*mid <= x && (mid+1)*(mid+1) > x)
+    			return mid;
+    		else if (mid * mid > x)
+    			r = mid - 1;
+    		else
+    			l = mid + 1;
+    	}
+    	return l;
+    }
+};
+
 int main() {
 	Solution s;
 	cout << s.mySqrt(pow(2, 31) - 1) << endl;
